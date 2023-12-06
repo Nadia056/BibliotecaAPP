016a6e8ece9528b7ff2bc85e017d65aec63f5022
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-12-05 23:18:41 -0600

    n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectorioComponent } from './directorio/directorio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectorioComponent
   ],
   imports: [
     BrowserModule,
